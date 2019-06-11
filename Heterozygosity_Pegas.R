4f830243ca6eedeefa483cb754ea13df1d79e469
Sirjan Sapkota <38262310+sirjansapkota@users.noreply.github.com>
2019-06-11 17:58:53 -0400

    Update and rename Heterozygosity_Pegas_r.R to Heterozygosity_Pegas.R

--- Heterozygosity_Pegas.R
+++ Heterozygosity_Pegas.R
@@ -1,13 +1,15 @@
-rm(list=ls())
-.libPaths(c('/Users/ssapkot/Documents/SAP/GenSel/MyRlibs',.libPaths()))
 
 library(pegas)
-setwd("/Users/ssapkot/Documents/GenomicSelection1/SAP_Yield/Additional_analysis/")
-hapmap <- read.table("/Users/ssapkot/Documents/GenomicSelection1/SAP_Yield/01_Data/SAP_GS_caudatum_all.hmp.txt",row.names=1, sep="\t", stringsAsFactors = FALSE)
+
+## read and format the hapmap file
+hapmap <- read.table("01_Data/SAP_GS_caudatum_all.hmp.txt",row.names=1, sep="\t", stringsAsFactors = FALSE)
 hmp <- as.data.frame(t(hapmap))
 hmp <- hmp[-c(1:10),]
+
+## extract the names of the snp sites
 sites <- as.data.frame(colnames(hmp))
-####pegas###heterozygosity for all sites
+
+## heterozygosity for all sites
 Result <- c()
 for (i in 2:ncol(hmp)) {
   het <- c()
